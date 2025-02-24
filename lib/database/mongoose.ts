ac2d28360d9aeb01cb8c30affe7928f8e309d230
HeliosMG4 <abhijithajay49@gmail.com>
2025-02-24 22:24:59 +0530

    first commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,18 +1,18 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL; // Fixed typo: MONGOBD_URL → MONGODB_URL
-
+const MONGODB_URL = process.env.MONGODB_URL;
 interface MongooseConnection {
     conn: Mongoose | null;
     promise: Promise<Mongoose> | null;
 }
 
 declare global {
-    // eslint-disable-next-line no-var
+   
+    
     var mongoose: MongooseConnection | undefined;
 }
 
-const cached: MongooseConnection = global.mongoose || {  // Added missing "||" operator
+const cached: MongooseConnection = global.mongoose || {  
     conn: null,
     promise: null,
 };
@@ -26,7 +26,7 @@ export const connectToDatabase = async () => {
 
     if (!MONGODB_URL) throw new Error(' Missing MONGODB_URL ');
 
-    cached.promise = cached.promise || // Added missing "||" operator
+    cached.promise = cached.promise || 
     mongoose.connect(MONGODB_URL, {
         dbName:'Pixperfect',
         bufferCommands: false
